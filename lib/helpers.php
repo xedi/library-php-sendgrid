514055b3bbc5bbad8d23763048225131dd7f8dad
Chris Smith <chris@xedi.com>
2019-09-18 14:01:31 +0100

    [dev] Cleaned up helpers

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,61 +1,15 @@
 <?php
 
-use Closure;
-
-if (! function_exists('class_uses_recursive')) {
-    /**
-     * Returns all traits used by a class, its parent classes and trait of their traits.
-     *
-     * @param  object|string  $class
-     *
-     * @return array
-     */
-    function class_uses_recursive($class)
-    {
-        if (is_object($class)) {
-            $class = get_class($class);
-        }
-
-        $results = [];
-
-        foreach (array_reverse(class_parents($class)) + [$class => $class] as $class) {
-            $results += trait_uses_recursive($class);
-        }
-
-        return array_unique($results);
-    }
-}
-
-if (! function_exists('trait_uses_recursive')) {
-    /**
-     * Returns all traits used by a trait and its traits.
-     *
-     * @param  string  $trait
-     *
-     * @return array
-     */
-    function trait_uses_recursive($trait)
-    {
-        $traits = class_uses($trait);
-
-        foreach ($traits as $trait) {
-            $traits += $trait_uses_recursive($trait);
-        }
-
-        return $traits;
-    }
-}
-
 if (! function_exists('array_every')) {
     /**
-     * Determine if all the items in an array pass the given test.
+     * Determine if all the items in an array pass the given truth test.
      *
-     * @param  array        $array    Array to be tested
-     * @param  Closure|null $callable Test
+     * @param  array         $array    Array to be tested
+     * @param  callable|null $callable Truth test
      *a
      * @return bool
      */
-    function array_every(array $array, Closure $callable = null): bool
+    function array_every(array $array, callable $callable = null): bool
     {
         $original_count = count($array);
         $filtered_count = count(
@@ -94,8 +48,8 @@ if (! function_exists('optional')) {
     /**
      * Provide access to optional objects.
      *
-     * @param  mixed  $value
-     * @param  callable|null  $callback
+     * @param  mixed         $value
+     * @param  callable|null $callback
      *
      * @return mixed
      */
