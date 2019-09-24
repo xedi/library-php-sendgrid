99b3462784bb8158765189f366e3eb56b4edd6e7
Chris Smith <chris@xedi.com>
2019-09-24 14:19:04 +0100

    [master] Fixed the remaining broken test in HandleClientExceptionTest

--- tests/Unit/Clients/Concerns/HandlesExceptions/HandleClientExceptionTest.php
+++ tests/Unit/Clients/Concerns/HandlesExceptions/HandleClientExceptionTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit\Clients\Concerns\HandlesExceptions;
 
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
 use Mockery;
 use Tests\Unit\Clients\Concerns\HandlesExceptions\Stub;
 use Tests\Unit\TestCase;
@@ -16,18 +18,28 @@ class HandleClientExceptionTest extends TestCase
      */
     public function handles400()
     {
-        $this->markTestSkipped('@CS - Test broken for some reason');
-
-        ($mock_exception = Mockery::mock(ClientException::class))
-            ->shouldReceive('getCode')
+        ($mock_response = Mockery::mock(GuzzleResponse::class))
+            ->shouldReceive('getStatusCode')
             ->once()
             ->andReturn(400);
+
+        $mock_request = Mockery::mock(GuzzleRequest::class);
+
+        $mock_exception = Mockery::mock(
+            ClientException::class,
+            [
+                'a-message',
+                $mock_request,
+                $mock_response
+            ]
+        )
+            ->makePartial();
 
         $mock_local_exception = Mockery::mock(ExceptionContract::class);
 
         /*
             This is really dirty, but I don't know a better way of testing this
-            in a strictly unit fashion.
+            in a strictly unit fashion; without compromising the class design.
 
             @CS - 23/09/2019
          */
